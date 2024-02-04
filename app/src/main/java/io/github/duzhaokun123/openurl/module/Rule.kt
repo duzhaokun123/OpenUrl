7d5f369105baadaa3fe20765eaa7c0a1a2a86472
o0kam1 <duzhaokun2@outlook.com>
2024-02-04 18:33:45 +0800

    fix: keep module

--- app/src/main/java/io/github/duzhaokun123/openurl/module/Rule.kt
+++ app/src/main/java/io/github/duzhaokun123/openurl/module/Rule.kt
@@ -1,12 +1,21 @@
 package io.github.duzhaokun123.openurl.module
 
+import com.google.gson.annotations.SerializedName
+
 data class Rule(
+    @SerializedName("name")
     val name: String,
+    @SerializedName("enabled")
     var enabled: Boolean = true,
+    @SerializedName("matches")
     var matches: String = "",
+    @SerializedName("regex")
     var regex: Boolean = false,
+    @SerializedName("mode")
     var mode: Int = MODE_DIRECT,
+    @SerializedName("targetPackage")
     var targetPackage: String = "",
+    @SerializedName("targetActivity")
     var targetActivity: String = ""
 ) {
     companion object {
